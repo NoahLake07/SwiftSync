90c485b9ec8d08e1c063eb38fa08d6f8cababe67
Noah Lake <noahlake07@gmail.com>
2023-02-14 07:44:33 -0700

    Initial Commit

---
+++ src/com/swiftsync/Pages.java
@@ -0,0 +1,7 @@
+package com.swiftsync;
+
+public interface Pages {
+
+    int DASHBOARD = 1000;
+
+}
---
+++ src/com/swiftsync/panels/Dashboard.java
@@ -0,0 +1,16 @@
+package com.swiftsync.panels;
+
+import freshui.graphics.FPanel;
+
+import javax.swing.*;
+
+public class Dashboard extends FPanel {
+
+    public Dashboard(){
+        super();
+
+        JLabel label = new JLabel();
+        this.add
+    }
+
+}
